2912dbd51381e8fd4a567a9b5c633eed13216b7e
Brian Ko <brianko42@gmail.com>
2022-10-26 23:14:34 -0700

    forgot to save file before last push, same as last push but with actual updated files

--- src/api/index.js
+++ src/api/index.js
@@ -67,24 +67,25 @@ export const getUserDetails = async (token) => {
 }
 
 // says no request parameter needed but probably needs an if statement or something or about the token being there
+// added in 'Authorization': `Bearer ${token}`
 export const getUserRoutines = async (token) => {
-    // try {
-    //     const response = await fetch(`${baseURL}/users/:username/routines`, {
-    //         headers: {
-    //             'Content-Type': 'application/json',
+    try {
+        const response = await fetch(`${baseURL}/users/:username/routines`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
 
-    //         },
+        })
 
-    //     })
+        const result = await response.json();
 
-    //     const result = await response.json();
+        return result;
 
-    //     return result;
-
-    // }
-    // catch (err) {
-    //     console.error('getUserRoutines-api/index.js FAILED:', err);
-    // }
+    }
+    catch (err) {
+        console.error('getUserRoutines-api/index.js FAILED:', err);
+    }
 }
 
 export const getAllActivities = async () => {
@@ -122,4 +123,74 @@ export const getPublicRoutines = async () => {
     catch (err) {
         console.error('getPublicRoutines-api/index.js FAILED:', err)
     }
+}
+
+export const createNewRoutine = async (token,{name, goal}) => {
+    try {
+        const response = await fetch (`${baseURL}/routines`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+        body: JSON.stringify({
+          name,
+          goal,
+          isPublic: true
+        })
+    })
+        const result = await response.json();
+        console.log ("-----New Routine-----", result);
+        return result;
+    }
+    catch (err) {
+        console.error('createNewRoutine-api/index.js FAILED:', err)
+    }
+}
+
+export const updateRoutine = async (token,{name, goal, isPublic, _id}) => {
+    try {
+        const response = await fetch (`${baseURL}/routines/${_id}`, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+        body: JSON.stringify({
+          name,
+          goal,
+          isPublic
+        })
+    })
+        .then(response => response.json())
+        .then(result => {
+        console.log(result);
+        })
+        .catch(console.error);
+    }
+    catch (err) {
+        console.error('createNewRoutine-api/index.js FAILED:', err)
+    }
+}
+
+
+export const deleteRoutine = async (token, {_id} ) => {
+    try {   
+        const response = await fetch (`${baseURL}/routines/${_id}`, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    })
+        .then(response => response.json())
+        .then(result => {
+        console.log(result);
+        })
+        .catch(console.error);
+    }
+    catch (err) {
+        console.error('deleteRoutine-api/index.js FAILED:', err)
+    }
+    
 }
